8a18d2ce53fdf6163676104f4419ee96b6db1ad9
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2020-04-18 19:48:57 -0700

    update home ui

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -6,10 +6,12 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HomeRoutingModule} from "./home-routing.module";
 import {HomeComponent} from "./home.component";
 import {SharedModule} from "../shared/shared.module";
+import { HomeItemComponent } from './home-item/home-item.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HomeItemComponent
   ],
   imports: [
     FormsModule,
